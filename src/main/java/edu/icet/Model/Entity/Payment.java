321147af7efcf4cdc6b60da33b74ca9cf2360a1a
Damitha Theekshana <theekshanadamitha@gmail.com>
2025-12-12 10:08:24 +0530

    add booking table paid bookings delete part to payment service method

--- src/main/java/edu/icet/Model/Entity/Payment.java
+++ src/main/java/edu/icet/Model/Entity/Payment.java
@@ -26,7 +26,7 @@ public class Payment {
 //    @JoinColumn(name = "booking_id", nullable = false)
 //    private Booking bookingEntity;
     @OneToOne
-    @JoinColumn(name = "booking_id", nullable = false)
+    @JoinColumn(name = "booking_id", nullable = true)
     private Booking bookingEntity;
 
 }
--- src/main/java/edu/icet/Service/PaymentService.java
+++ src/main/java/edu/icet/Service/PaymentService.java
@@ -1,11 +1,18 @@
 package edu.icet.Service;
 
-import edu.icet.Model.Dto.PaymentDTO;import edu.icet.Repository.BookingRepository;
+import edu.icet.Model.Dto.BookingResponseDto;
+import edu.icet.Model.Dto.PaymentDTO;
+import edu.icet.Model.Entity.Booking;
+import edu.icet.Model.Entity.Payment;
+import edu.icet.Model.Entity.Vehicle;
+import edu.icet.Repository.BookingRepository;
 import edu.icet.Repository.PaymentRepository;
 import edu.icet.Repository.VehicleRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PaymentService {
@@ -23,6 +30,29 @@ public class PaymentService {
 
 
     public void addPayment(PaymentDTO dto) {
+        // 1. Fetch booking
+        Booking booking = bookingRepository.findById(dto.getBookingId())
+                .orElseThrow(() -> new RuntimeException("Booking not found"));
 
+        // 2. Save payment
+        Payment payment = new Payment();
+        mapper.map(dto, payment);
+        payment.setAmount(dto.getAmount());
+        payment.setType("Cash");
+        payment.setStatus("PAID");
+        payment.setPaidDate(LocalDateTime.now());
+        payment.setBookingEntity(booking);
+
+        paymentRepository.save(payment);
+
+        // 3. Update vehicle status
+        Vehicle vehicle = booking.getVehicle();
+        vehicle.setStatus("AVAILABLE");
+        vehicleRepository.save(vehicle);
+
+        payment.setBookingEntity(null);
+        paymentRepository.save(payment);
+
+        bookingRepository.delete(booking);
     }
 }
